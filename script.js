dcf67220932282730a41067a2899f0d94e5cf9f9
ming5chiu <99941211+ming5chiu@users.noreply.github.com>
2024-05-22 17:29:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -83,11 +83,3 @@
 	
 })(jQuery); 
 
-	$(".pricing-opt").on('click', function () {
-		if ($("body").hasClass("pricing-opt-body-2")) {
-			$("body").removeClass("pricing-opt-body-2");
-		}
-		else {
-			$("body").addClass("pricing-opt-body-2");
-		}
-	});
